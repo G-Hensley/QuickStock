5549cbe56ad253a3112ff95e7179966aa5b7dc37
Gavin Hensley <gavinhensley@protonmail.com>
2025-01-02 21:00:10 -0500

    Update node modules

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+import { Request, Response } from "express";
 
 dotenv.config();
 
@@ -10,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 const PORT: number = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello World!!! First Server!");
+})
 
 app.listen(PORT, () => {
     console.log(`Server is runnning on port: ${PORT}`);
